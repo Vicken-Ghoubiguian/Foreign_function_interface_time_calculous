86c0bb9216989d68eaea1e689ded0fadd221e1e0
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-03-26 19:56:00 +0100

    Tbas

--- python/tkinter_sample/main.py
+++ python/tkinter_sample/main.py
@@ -15,4 +15,24 @@ root.iconbitmap('logo.ico')
 root.geometry("500x500")
 
 #
+tabs = ttk.Notebook(root)
+
+#
+tabl = ttk.Frame(tabs)
+tab2 = ttk.Frame(tabs)
+tab3 = ttk.Frame(tabs)
+tab4 = ttk.Frame(tabs)
+tab5 = ttk.Frame(tabs)
+
+#
+tabs.add(tabl, text="....1....")
+tabs.add(tab2, text="....2....")
+tabs.add(tab3, text="....3....")
+tabs.add(tab4, text="....4....")
+tabs.add(tab5, text="....5....")
+
+#
+tabs.pack(expand=1, fill='both')
+
+#
 root.mainloop()
