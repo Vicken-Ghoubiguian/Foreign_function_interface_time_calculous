2e952b8eaec1706cf7f85495542fdf07d9b86c74
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-03-27 19:28:07 +0100

    Later for tabs

--- python/tkinter_sample/main.py
+++ python/tkinter_sample/main.py
@@ -31,6 +31,18 @@ tabs.add(tab2, text="Calculations on date and time")
 tabs.add(tab3, text="Number of weeks in a year according to the iso norm")
 
 #
+l1=ttk.Label(tabl,text="I am tab-1",width=10)
+l1.place(relx=0.4,rely=0.2)
+
+#
+l2=ttk.Label(tab2,text="I am tab-2",width=10)
+l2.place(relx=0.4,rely=0.2)
+
+#
+l3=ttk.Label(tab3,text="I am tab-3",width=10)
+l3.place(relx=0.4,rely=0.2)
+
+#
 tabs.pack(expand=1, fill='both')
 
 #
