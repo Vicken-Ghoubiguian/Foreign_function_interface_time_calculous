a53dc94d2e3f54f3b73d81fe657c5e1af8cae5f6
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-03-31 20:33:11 +0200

    For future 'time_calculation' integration [python]

---
+++ python/tkinter_sample/build_time_calculous.py
@@ -0,0 +1,24 @@
+#
+from cffi import FFI
+
+#
+ffibuilder = FFI()
+
+ffibuilder.cdef("""
+ double sqrt(double x); // list all the function prototypes from the
+ double sin(double x); // library that we want to use
+ """)
+
+
+#
+ffibuilder.set_source("_time_calculous",
+"""
+ #include <math.h> // the C header of the library
+""",
+ library_dirs = [], # here we can provide where the library is located,
+ # as we are using C standard library empty list is enough
+ libraries = ['m'] # name of the library we want to interface
+)
+
+#
+ffibuilder.compile(verbose=True)
