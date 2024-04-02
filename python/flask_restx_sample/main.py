e9eb2c63e1865b62ba2c8d9265a0b4a80d1853d0
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-04-02 17:38:51 +0200

    Flask-rest-x in It

---
+++ python/flask_restx_sample/main.py
@@ -0,0 +1,13 @@
+from Flask import Flask
+from flask_restx import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+if __name__ == '__main__':
+    app.run(debug=True)
